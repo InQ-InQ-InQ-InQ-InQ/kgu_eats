8f2f1862a3ecf0e1bfaa8de1be1d073c77e75044
dbguswls030 <dbguswls030@gmail.com>
2021-12-24 04:05:51 +0900

    Create IOS project & UI design

---
+++ ios/kgu_eats/kgu_eats/RestaurantInfoView.swift
@@ -0,0 +1,29 @@
+//
+//  RestaurantInfoView.swift
+//  kgu_eats
+//
+//  Created by 유현진 on 2021/12/24.
+//
+
+import UIKit
+
+class RestaurantInfoView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ios/kgu_eats/kgu_eats/SelectRestaurantView.swift
@@ -0,0 +1,29 @@
+//
+//  SelectRestaurantView.swift
+//  kgu_eats
+//
+//  Created by 유현진 on 2021/12/23.
+//
+
+import UIKit
+
+class SelectRestaurantView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
