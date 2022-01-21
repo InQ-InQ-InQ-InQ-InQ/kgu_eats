360a926582e1b9225d6ebf5f8d50e4e8001a18b1
sinb57 <sinb57@naver.com>
2022-01-21 17:26:46 +0900

    feat: 매장 목록 반환 데이터 수정 (불필요한 데이터 축소)

--- backend/src/main/java/kgueats/domain/store/model/dto/StoreSimpleDto.java
+++ backend/src/main/java/kgueats/domain/store/model/dto/StoreSimpleDto.java
@@ -1,12 +1,8 @@
 package kgueats.domain.store.model.dto;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import lombok.Builder;
 import lombok.Getter;
 
-import kgueats.domain.store.model.entity.BusinessHour;
 import kgueats.domain.store.model.entity.CampusEnum;
 import kgueats.domain.store.model.entity.Store;
 
@@ -14,28 +10,21 @@ import kgueats.domain.store.model.entity.Store;
 public class StoreSimpleDto {
 
     private final Long id;
-    private final List<BusinessHourDto> businessHours;
     private final Long campusId;
     private final String name;
-    private final String location;
 
     @Builder
-    public StoreSimpleDto(Long id, List<BusinessHour> businessHours,
-                          CampusEnum campus, String name, String location) {
+    public StoreSimpleDto(Long id, CampusEnum campus, String name) {
         this.id = id;
-        this.businessHours = businessHours.stream().map(BusinessHourDto::toDto).collect(Collectors.toList());
         this.campusId = campus.getId();
         this.name = name;
-        this.location = location;
     }
 
     public static StoreSimpleDto toDto(Store store) {
         return StoreSimpleDto.builder()
                 .id(store.getId())
-                .businessHours(store.getBusinessHours())
                 .campus(store.getCampus())
                 .name(store.getName())
-                .location(store.getLocation())
                 .build();
     }
 
