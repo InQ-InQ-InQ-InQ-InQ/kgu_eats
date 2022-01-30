519294354b08724da15a401ccc7c4babe7c684a8
sinb57 <sinb57@naver.com>
2022-01-30 21:53:13 +0900

    fix: Review 엔티티 @JoinColumn 누락

--- backend/src/main/java/kgueats/domain/review/model/entity/Review.java
+++ backend/src/main/java/kgueats/domain/review/model/entity/Review.java
@@ -8,6 +8,7 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -30,9 +31,11 @@ public class Review {
 	private Long id;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "student_id")
 	private Student student;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "menu_id")
 	private Menu menu;
 
 	private String content;
